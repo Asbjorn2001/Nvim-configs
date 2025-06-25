03f05a0880fce87492775b87f1fbaa3d7a3e6119
AserDevel <asbjorn.biering@gmail.com>
2025-06-25 14:43:55 +0200

    Removed API key

--- lua/plugins/gp.lua
+++ lua/plugins/gp.lua
@@ -2,8 +2,7 @@ return {
     "robitx/gp.nvim",
     config = function()
         require("gp").setup({
-            -- openai_api_key = os.getenv("OPEN_API_KEY"),
-            openai_api_key = "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",
+            -- openai_api_key = os.getenv("OPEN_API_KEY"), 
         })
     end,
 }
